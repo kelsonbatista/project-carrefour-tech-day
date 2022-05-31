3174dd0783ad64d6f039533e4af9d6a56bd9ae26
kelsonbatista <kelsonbatista@gmail.com>
2022-05-31 14:57:52 -0300

    feat(app.js): added basic handleSellers function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
+import fetchSellersAPI from "./services/sellersAPI";
 
-function App() {
+const App = () => {
+  const [sellers, setSellers] = useState([]);
+
+  const handleSellers = async (country, postalcode) => {
+    const getSellers = await fetchSellersAPI(country, postalcode);
+    console.log(getSellers);
+    setSellers(getSellers);
+  };
+
+  useEffect(() => {
+    handleSellers("BRA", "12243000");
+  }, []);
+
   return (
     <>
       <Header />
       <main>
-        <></>
+        {sellers.map((seller) => (
+          <li key={seller.id}>{seller.name}</li>
+        ))}
       </main>
     </>
   );
-}
+};
 
 export default App;
