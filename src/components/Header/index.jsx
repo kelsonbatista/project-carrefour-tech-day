8eaede06d18df02088b97152363972f3e79b4481
kelsonbatista <kelsonbatista@gmail.com>
2022-05-31 13:49:30 -0300

    feat(components): added Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import logo from "../../assets/images/logo-carrefour-site.png";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt="Carrefour" />
+    </header>
+  );
+};
+
+export default Header;
