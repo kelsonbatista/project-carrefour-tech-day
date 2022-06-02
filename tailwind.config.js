f76c85354720d4b443e8df9bb09faef99bb76712
kelsonbatista <kelsonbatista@gmail.com>
2022-06-02 02:27:37 -0300

    feat(tailwind): added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-blue-carrefour": "#1E5BC6",
+        "bg-red-carrefour": "#FC0003",
+        "bg-blue-300": "#93c5fd",
+        "bg-blue-500": "#3b82f6",
+        "bg-blue-700": "#1d4ed8",
+        "bg-yellow-300": "#fde047",
+        "bg-green-300": "#86efac",
+      },
+    },
   },
   plugins: [],
 };
