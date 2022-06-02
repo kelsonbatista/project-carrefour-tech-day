8b0db52d3f48b1881bbb08c6752666a1e73db0ff
kelsonbatista <kelsonbatista@gmail.com>
2022-06-02 02:23:57 -0300

    feat(header): added basic structure to header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,31 @@
 import logo from "../../assets/images/logo-carrefour-site.png";
+import "./styles.css";
 
 const Header = () => {
   return (
     <header>
-      <img src={logo} alt="Carrefour" />
+      <section className="header">
+        <div>
+          <img src={logo} alt="Carrefour" className="header__logo" />
+        </div>
+        <div className="header__search input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Recipient's username"
+            aria-label="Recipient's username"
+            aria-describedby="button-addon2"
+          />
+          <button
+            className="btn btn-outline-secondary"
+            type="button"
+            id="button-addon2"
+          >
+            <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+          </button>
+        </div>
+        <div className="header__icons">Icons</div>
+      </section>
     </header>
   );
 };
