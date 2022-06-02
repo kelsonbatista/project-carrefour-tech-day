fc03c81135f26f8b9ea999a7f873d7eaaff4bd84
kelsonbatista <kelsonbatista@gmail.com>
2022-06-02 20:20:15 -0300

    refactor(header): added react-icons and new layout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,37 @@
-import {
-  faBox,
-  faCartShopping,
-  faMagnifyingGlass,
-  faUser,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { AiOutlineMenu } from "react-icons/ai";
+import { BiUser } from "react-icons/bi";
+import { BsBoxSeam, BsCart2, BsHeart } from "react-icons/bs";
+import { GrSearch } from "react-icons/gr";
+import bannerTop from "../../assets/images/banner-top-1.png";
 import logo from "../../assets/images/logo-carrefour-site.png";
 import "./styles.css";
 
 const Header = () => {
   return (
     <header>
+      <section className="header__banner">
+        <img
+          src={bannerTop}
+          alt="Banner Promo"
+          className="header__banner-top"
+        />
+      </section>
       <section className="header">
         <div className="header__logo">
-          <img src={logo} alt="Carrefour" className="header__logo-img" />
+          <div className="header__menu">
+            <AiOutlineMenu
+              className="header__menu-sm"
+              size="30px"
+              color="black"
+            />
+            <img src={logo} alt="Carrefour" className="header__logo-img" />
+          </div>
+          <div className="header__icons-sm">
+            <BiUser size="30px" color="blue" />
+            <BsBoxSeam size="30px" color="blue" />
+            <BsHeart size="30px" color="blue" />
+            <BsCart2 size="30px" color="blue" />
+          </div>
         </div>
         <div className="header__search input mb-3">
           <input
@@ -23,13 +41,14 @@ const Header = () => {
             aria-label="Pesquise por produtos ou marcas"
           />
           <button className="header__button btn btn-primary">
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
+            <GrSearch color="white" />
           </button>
         </div>
         <div className="header__icons">
-          <FontAwesomeIcon icon={faUser} />
-          <FontAwesomeIcon icon={faBox} />
-          <FontAwesomeIcon icon={faCartShopping} />
+          <BiUser size="30px" color="blue" />
+          <BsBoxSeam size="30px" color="blue" />
+          <BsHeart size="30px" color="blue" />
+          <BsCart2 size="30px" color="blue" />
         </div>
       </section>
     </header>
