87f72097ca3392552227495c5db6818e9f9b15e2
kelsonbatista <kelsonbatista@gmail.com>
2022-06-01 22:01:08 -0300

    feat(redux): added Actions index file

---
+++ src/store/actions/index.js
@@ -0,0 +1,5 @@
+import setLoading from "./loadingAction";
+import setProducts from "./productAction";
+import setProductsSeller from "./sellerAction";
+
+export { setProductsSeller, setLoading, setProducts };
