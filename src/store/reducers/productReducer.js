c45a2a47dace5f6a2bbe2a51960bc0cc18098f67
kelsonbatista <kelsonbatista@gmail.com>
2022-06-03 00:59:43 -0300

    fix(productreducer): productsReducer attributes modified

--- src/store/reducers/productReducer.js
+++ src/store/reducers/productReducer.js
@@ -1,16 +1,22 @@
 import { SET_FILTER, SET_PRODUCTS } from "../actions/productAction";
 
 const INITIAL_STATE = {
-  products: [],
+  list: [],
   filter: "",
 };
 
 const productsReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case SET_PRODUCTS:
-      return action.payload;
+      return {
+        ...state,
+        list: action.payload,
+      };
     case SET_FILTER:
-      return action.payload;
+      return {
+        ...state,
+        filter: action.payload,
+      };
     default:
       return state;
   }
