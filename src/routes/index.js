2f3bd2fbc951dbd51bf7f1be75d5ce2a55efa293
kelsonbatista <kelsonbatista@gmail.com>
2022-06-05 12:59:55 -0300

    feat(routes): added product item and not found routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route, Switch, withRouter } from "react-router-dom";
 import App from "../App";
+import NotFound from "../components/NotFound";
+import Product from "../pages/Product";
 
 const RoutesApp = () => {
   return (
     <Switch>
       <Route exact path="/" component={App} />
+      <Route exact path="/product/:productId" component={Product} />
+      <Route path="*" component={NotFound} />
     </Switch>
   );
 };
