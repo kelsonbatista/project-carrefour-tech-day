16e6201e4b936bee2b2ca407c0699506bdd80c62
kelsonbatista <kelsonbatista@gmail.com>
2022-06-05 13:19:00 -0300

    feat(components): added Not Found component

---
+++ src/components/NotFound/index.jsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <section className="notfound">
+      <h1>Erro 404</h1>
+      <h2>Página não encontrada</h2>
+    </section>
+  );
+};
+
+export default NotFound;
