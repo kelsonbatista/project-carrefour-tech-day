be10648252eef4cf765a85bf0f1e25d051a25ed4
kelsonbatista <kelsonbatista@gmail.com>
2022-06-05 13:22:20 -0300

    feat(loading): added css style tag to loading component

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,12 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { Spinner } from "react-bootstrap";
+import "./styles.css";
 
 const Loading = () => {
   return (
-    <>
+    <div className="loading">
       <Spinner animation="border" variant="primary" />
-    </>
+    </div>
   );
 };
 
