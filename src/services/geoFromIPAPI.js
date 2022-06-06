e358dccb94bafd9353a9ca2b7e4d97cc53d2942a
kelsonbatista <kelsonbatista@gmail.com>
2022-06-05 22:15:51 -0300

    feat(api): added API to get geolocation from IP

---
+++ src/services/geoFromIPAPI.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const fetchGeoFromIP = async (setIsLoading) => {
+  const TOKEN = "ab9573fc48719d";
+  const URL = `https://ipinfo.io?token=${TOKEN}`;
+  setIsLoading(false);
+  return await axios
+    .get(URL)
+    .then((result) => result.data)
+    .catch((err) => `Error: ${err.message}`);
+};
+
+export default fetchGeoFromIP;
