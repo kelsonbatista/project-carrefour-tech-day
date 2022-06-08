984b0ab89a044a2228d26a40482bebf86806ecba
kelsonbatista <kelsonbatista@gmail.com>
2022-06-08 12:14:47 -0300

    refactor(cartbutton): change the way of getting product quantity

--- src/components/CartButton/index.jsx
+++ src/components/CartButton/index.jsx
@@ -1,34 +1,32 @@
 import PropTypes from "prop-types";
 import { BsCart2 } from "react-icons/bs";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import "./styles.css";
 
 const CartButton = (props) => {
-  const { cartItemsQty } = props;
-  const updateCart = () => {
-    const cart = localStorage["carrefour-cart"]
-      ? JSON.parse(localStorage["carrefour-cart"])
-      : [];
-    if (cart.length > 0) {
-      const qtyItems = cart.reduce((acc, item) => acc + item.qty, 0);
-      return qtyItems || qtyItems + cartItemsQty;
-    }
-  };
+  const { cartQty } = props;
 
   return (
     <div className="cart__btn-div">
-      <Link to="/cart" className="cart__btn" data-testid="shopping-cart-button">
+      <Link to="/" className="cart__btn" data-testid="shopping-cart-button">
         <BsCart2 size="30px" color="blue" />
       </Link>
-      <div className="cart__btn-qty">
-        <p data-testid="shopping-cart-size">{updateCart()}</p>
-      </div>
+      {cartQty && cartQty !== "0" && (
+        <div className="cart__btn-qty">
+          <p data-testid="shopping-cart-size">{cartQty}</p>
+        </div>
+      )}
     </div>
   );
 };
 
 CartButton.propTypes = {
-  cartItemsQty: PropTypes.number,
+  cartQty: PropTypes.number,
 }.isRequired;
 
-export default CartButton;
+const mapStateToProps = (state) => ({
+  cartQty: state.products.cartTotal.qty,
+});
+
+export default connect(mapStateToProps, null)(CartButton);
