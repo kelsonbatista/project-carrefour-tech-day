febdfa10207e6ae508fb6d1f0a1b8ecc89021d66
kelsonbatista <kelsonbatista@gmail.com>
2022-06-08 23:41:31 -0300

    feat(cartsidebar): created cart sidebar component and css styles

---
+++ src/components/CartSideBar/index.jsx
@@ -0,0 +1,94 @@
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import { Button, Image } from "react-bootstrap";
+import { AiOutlineClose } from "react-icons/ai";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { setCartOpen } from "../../store/actions";
+import "./styles.css";
+
+const CartSideBar = (props) => {
+  const [products, setProducts] = useState([]);
+  const { qtyTotal, total, cartOpen, dispatchCartOpen } = props;
+
+  const getCart = () => {
+    const cart = localStorage["carrefour-cart"]
+      ? JSON.parse(localStorage["carrefour-cart"])
+      : [];
+    if (cart.length > 0) {
+      console.log(cart.length, "WWWWWWWWWWWWWWWWWWWWW");
+      setProducts(cart);
+    }
+  };
+
+  useEffect(() => {
+    getCart();
+    console.log(qtyTotal, "QQQQQQQQQQQQQQQQ");
+  }, [qtyTotal]);
+
+  return (
+    <div className={`cartsidebar ${cartOpen && "active"}`}>
+      {console.log(cartOpen && "active", "OPENNNNNNNNNNNNNN<<<<<<")}
+      <div className="cartsidebar__top">{`Carrinho (${qtyTotal})`}</div>
+      <div className="cartsidebar__items">
+        {products &&
+          products.map((product) => (
+            <div key={product.id} className="cartsidebar__item">
+              <div className="cartsidebar__image">
+                <Image src={product.url} alt={product.title} />
+              </div>
+              <div className="cartsidebar__title">{product.title}</div>
+              <div className="cartsidebar__qty">{product.qty}</div>
+              <div className="cartsidebar__price">
+                {`R$ ${product.price.toFixed(2)}`}
+              </div>
+            </div>
+          ))}
+      </div>
+      <div className="cartsidebar__bottom">
+        <div className="cartsidebar__summary">
+          <div className="cartsidebar__qty-total">{`Subtotal (${qtyTotal} produtos)`}</div>
+          <div className="cartsidebar__total">{`R$ ${total}`}</div>
+        </div>
+        <div className="cartsidebar__checkout">
+          <Button
+            className="cartsidebar__checkout-btn btn btn-success"
+            type="button"
+            onClick={() => {}}
+            id="basic-addon2"
+          >
+            {"Finalizar compra"}
+          </Button>
+        </div>
+      </div>
+      <div className="cartsidebar__close">
+        <Link
+          to="#"
+          className="cart__btn"
+          data-testid="shopping-cart-button"
+          onClick={() => dispatchCartOpen(false)}
+        >
+          <AiOutlineClose size="30px" />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+CartSideBar.propTypes = {
+  qtyTotal: PropTypes.number,
+  total: PropTypes.number,
+  cartOpen: PropTypes.bool,
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  qtyTotal: state.products.cartTotal.qty,
+  total: state.products.cartTotal.total,
+  cartOpen: state.products.cartOpen,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  dispatchCartOpen: (close) => dispatch(setCartOpen(close)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartSideBar);
